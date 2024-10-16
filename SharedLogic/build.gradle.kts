49451bd34b4f71a7477e8ae9c5a5401f6710af43
MLGPenguin <baww123@gmail.com>
2024-10-16 17:28:21 +1000

    doc

--- SharedLogic/build.gradle.kts
+++ SharedLogic/build.gradle.kts
@@ -10,5 +10,8 @@ repositories {
 }
 
 dependencies {
-    // api("/* KTOR */")
+    // IF the downstream modules (spigot/velocity/bungee) need access to KTOR dependency,
+    // change 'implementation' to 'api'. But for better build performance try to abstract KTOR methods.
+
+    // implementation("/* KTOR */")
 }
